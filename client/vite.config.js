b31b6ddea734844a3d8db0a1ab974cbe4aef7d4d
Will <86987398+Will6855@users.noreply.github.com>
2026-01-07 18:43:38 +0100

    chore: add analytics script to production build via Vite plugin

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,28 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(), 
+    tailwindcss(),
+    {
+      name: 'html-transform',
+      transformIndexHtml(html) {
+        if (process.env.NODE_ENV === 'production') {
+          return [
+            {
+              tag: 'script',
+              attrs: {
+                defer: true,
+                src: 'https://51.210.6.50:3010/script.js',
+                'data-website-id': '472a48fd-8c86-4a48-8cbf-3b816f37c944'
+              },
+              injectTo: 'head'
+            }
+          ];
+        }
+      }
+    }
+  ],
   server: {
     port: 3006,
     strictPort: true
